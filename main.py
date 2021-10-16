d27099130bd7407d89a7d9d96012a13e8c575299
ulwlu <ooulwluoo@gmail.com>
2021-10-16 16:36:34 +0900

    Day2

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
+from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class ClosedRange:
+class Range(metaclass=ABCMeta):
     lower_endpoint: int
     upper_endpoint: int
 
@@ -12,3 +15,22 @@ class ClosedRange:
 
     def __str__(self) -> str:
         return f"[{self.lower_endpoint},{self.upper_endpoint}]"
+
+    @abstractmethod
+    def contains(self, target: int) -> bool:
+        pass
+
+    def is_connected_to(self, target: ClosedRange) -> bool:
+        return self.contains(target.lower_endpoint) or self.contains(
+            target.upper_endpoint
+        )
+
+
+class ClosedRange(Range):
+    def contains(self, target: int) -> bool:
+        return self.lower_endpoint <= target <= self.upper_endpoint
+
+
+class OpenedRange(Range):
+    def contains(self, target: int) -> bool:
+        return self.lower_endpoint < target < self.upper_endpoint
--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import pytest
 from black import nullcontext
 
-from main import ClosedRange
+from main import ClosedRange, OpenedRange
 
 
-class TestClosedRange:
+class TestRange:
     @pytest.fixture
     def closedrange(self):
         return ClosedRange(3, 8)
+
+    @pytest.fixture
+    def openedrange(self):
+        return OpenedRange(3, 8)
 
     def test_閉区間から下端点を取得できること(self, closedrange):
         assert closedrange.lower_endpoint == 3
@@ -26,8 +30,37 @@ class TestClosedRange:
             (8, 3, pytest.raises(ValueError)),
         ],
     )
-    def test_下端点が上端点より必ず小さいこと(
-        self, lower_endpoint, upper_endpoint, expected, closedrange
-    ):
+    def test_下端点が上端点より必ず小さいこと(self, lower_endpoint, upper_endpoint, expected):
         with expected:
             ClosedRange(lower_endpoint, upper_endpoint)
+
+    @pytest.mark.parametrize(("target, expect"), [(5, True), (-1, False)])
+    def test_閉区間が任意の整数を含むか(self, closedrange, target, expect):
+        assert closedrange.contains(target) == expect
+
+    @pytest.mark.parametrize(
+        ("lower_endpoint, upper_endpoint, expected"),
+        [
+            (3, 8, True),
+            (1, 6, False),
+        ],
+    )
+    def test_任意の下端点と上端点が等しい閉区間は等しいか(
+        self, lower_endpoint, upper_endpoint, expected, closedrange
+    ):
+        assert (closedrange == ClosedRange(lower_endpoint, upper_endpoint)) == expected
+
+    @pytest.mark.parametrize(
+        ("lower_endpoint, upper_endpoint, expected"),
+        [
+            (1, 6, True),
+            (8, 15, True),
+            (9, 12, False),
+        ],
+    )
+    def test_任意の2つの閉区間が接続しているか(
+        self, lower_endpoint, upper_endpoint, expected, closedrange
+    ):
+        assert (
+            closedrange.is_connected_to(ClosedRange(lower_endpoint, upper_endpoint))
+        ) == expected
